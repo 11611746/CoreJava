793b4134267e92e89808e6d224cec5155a5b4044
PaulDSS <bishaldutta64@gmail.com>
2020-08-28 19:45:04 +0530

    Initial Commit

---
+++ IfElse.java
@@ -0,0 +1,27 @@
+/**
+*If-Else
+*It needs a specefic condition to work with it, either the conditions must be true or false.
+*We need to specify the condition in the 'IF' part and if the condition matches the 'IF' block will execute.
+*The 'ELSE' part has no condition in it, this 'ELSE' block works only if the 'IF' part condition is false.
+*/
+
+public class IfElse{
+  public static void main(String[] args) {
+    int number1 = 3;
+    int number2 = 1;
+
+    if (number1==3) {
+      System.out.println("Yes it is 3");
+    }
+    else{
+      System.out.println("No it's not 3");
+    }
+
+    if (number2==6) {
+      System.out.println("Yes it is 6");
+    }
+    else{
+      System.out.println("No it's not 6");
+    }
+  }
+}
