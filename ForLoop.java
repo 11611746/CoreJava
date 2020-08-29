75bb076d7f62fae82a5b01edc193800478043eae
PaulDSS <bishaldutta64@gmail.com>
2020-08-29 15:23:52 +0530

    Initial Commit

---
+++ ForLoop.java
@@ -0,0 +1,16 @@
+/**
+*For loop: Simlar to While loop but the difference is in the syntax part. If you see while loop the initilization, condition and increment/decrement part is done
+differently. So, in for loop all the stuff are put together.
+Eg:
+  for(initilization;condition;increment/decrement){
+  statements;
+}
+*/
+
+public class ForLoop{
+  public static void main(String[] args) {
+    for (int number=0;number<=10 ;number++ ) {
+      System.out.print(number+" ");
+    }
+  }
+}
